98d8563dd46ba67ea47fb071de7f8ff610544ef9
Sina <sinakmi70@gmail.com>
2022-05-19 23:02:32 +0430

    Add a hook for login api

---
+++ Backend/Api/Hooks/authenticate.py
@@ -0,0 +1,35 @@
+
+
+from Api.api_tools import ApiTools
+from Database.db_handler import Database
+
+HOST = 'localhost'
+PORT = 27017
+DB_NAME = 'sika-messenger'
+
+
+class Authenticate:
+
+    def __call__(self, req, resp, resource, params) -> None:
+        """
+        This magic method checks user credential.
+        """
+        user_credential = ApiTools.extract_auth_data(req.auth)
+
+        with Database(HOST, PORT, DB_NAME, 'users') as db:
+            db: Database
+            user = db.get_record({"email": user_credential['email']})
+
+        try:
+            is_match = ApiTools.check_password(
+                user_credential['password'],
+                user['password']
+            )
+            if is_match:
+                req.is_auth = True
+                req.user = user
+            else:
+                req.is_auth = False
+        except TypeError as e:
+            # This code runs when a user does not exists.
+            req.is_auth = -1
--- Backend/Api/Resources/users.py
+++ Backend/Api/Resources/users.py
@@ -1,9 +1,8 @@
-from re import M
-from typing import Collection
-import falcon
-from ..api_tools import ApiTools
-from Database.db_handler import Database
 from pprint import pprint
+import falcon
+from Api.api_tools import ApiTools
+from Database.db_handler import Database
+from Api.Hooks.authenticate import Authenticate
 
 HOST = 'localhost'
 PORT = 27017
@@ -12,37 +11,27 @@ DB_NAME = 'sika-messenger'
 
 class Users:
 
+    @falcon.before(Authenticate())
     def on_get_login(self, req: falcon.Request, resp: falcon.Response):
         """
         This method checks the user credential and if valid
-        return a valid response for authentication
+        return a valid response for authentication and the user data.
+
         """
 
-        user_credential = ApiTools.extract_auth_data(req.auth)
-
-        with Database(HOST, PORT, DB_NAME, 'users') as db:
-            db: Database
-            user = db.get_record({"email": user_credential['email']})
-        try:
-            is_match = ApiTools.check_password(
-                user_credential['password'],
-                user['password']
-            )
-
-            if is_match:
-                user = ApiTools.prepare_data_before_send(user)
-                resp.media = {
-                    "status": "ok",
-                    "message": "User is authenticated and can login.",
-                    "user": user
-                }
-            else:
-                resp.media = {
-                    "status": "error",
-                    "message": "Wrong password"
-                }
-        except TypeError as e:
-            print(e)
+        if req.is_auth == True:
+            user = ApiTools.prepare_data_before_send(req.user)
+            resp.media = {
+                "status": "ok",
+                "message": "User is authenticated and can login.",
+                "user": user
+            }
+        elif req.is_auth == False:
+            resp.media = {
+                "status": "error",
+                "message": "Wrong password."
+            }
+        else:
             resp.media = {
                 "status": "error",
                 "message": "User does not exists."

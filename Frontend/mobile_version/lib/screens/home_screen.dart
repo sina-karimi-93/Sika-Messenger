7666911837482d496a525d8b356eca75cdd94213
Sina <sinakmi70@gmail.com>
2022-05-28 19:40:04 +0430

    Start implementing channels chats

--- Frontend/mobile_version/lib/screens/home_screen.dart
+++ Frontend/mobile_version/lib/screens/home_screen.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_version/widgets/home_screen/private_chats.dart';
 import '../widgets/app_drawer.dart';
-import '../widgets/home_screen/header.dart';
-import '../widgets/home_screen/chat_item.dart';
+import '../widgets/home_screen/header_item.dart';
+import '../widgets/home_screen/private_chats.dart';
+import '../widgets/home_screen/channels.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
   static const routeName = 'home-screen';
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   final String status = "Connecting...";
+  Map navigation = {
+    "privates": false,
+    "groups": false,
+    "channels": true,
+  };
+
+  void navigationHandler({required String navigationKey}) {
+    /*
+    This method change the status of the navigation in Header.
+    User by tapping on each HeaderItem select them like a RadioBox.
+
+    args:
+      navigationKey: name of a key in navigation variable which
+                     comes from HeaderItem gesturedetector.
+    */
+    setState(() {
+      navigation = {
+        "privates": false,
+        "groups": false,
+        "channels": false,
+      };
+      navigation[navigationKey] = true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,8 +50,36 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          const Header(),
-          PrivateChats(),
+          Container(
+            height: 50,
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primary,
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30),
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                HeaderItem(
+                    title: "Privates",
+                    isSelected: navigation["privates"],
+                    navigationHandler: navigationHandler),
+                HeaderItem(
+                    title: "Groups",
+                    isSelected: navigation["groups"],
+                    navigationHandler: navigationHandler),
+                HeaderItem(
+                    title: "Channels",
+                    isSelected: navigation["channels"],
+                    navigationHandler: navigationHandler),
+              ],
+            ),
+          ),
+          if (navigation["privates"]) const PrivateChats(),
+          if (navigation["channels"]) const Channels(),
         ],
       ),
     );
---
+++ Frontend/mobile_version/lib/widgets/home_screen/channels.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+class Channels extends StatelessWidget {
+  const Channels({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
